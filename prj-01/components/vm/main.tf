2fcc8d337b7cad55d43883e727471cf2c9f4fe4d
vnnimbalkar <vaibhav.nimbalkar291@gmail.com>
2024-01-23 12:06:32 +0530

    Update main.tf

--- prj-01/components/vm/main.tf
+++ prj-01/components/vm/main.tf
@@ -26,11 +26,3 @@ resource "google_compute_instance" "default" {
     }
   }
 }
-
-resource "google_storage_bucket" "auto-expire" {
-  name          = "prj-01-bucket"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-}
