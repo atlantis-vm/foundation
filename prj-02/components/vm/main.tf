44b824a4fbdccaaa827d0b1316e3d81bab1e7d54
vnnimbalkar <vaibhav.nimbalkar291@gmail.com>
2024-01-23 12:56:47 +0530

    Update main.tf

--- prj-02/components/vm/main.tf
+++ prj-02/components/vm/main.tf
@@ -3,26 +3,3 @@ provider "google" {
   region      = var.region
 }
 
-resource "google_compute_instance" "default" {
-  name         = var.instance_name
-  machine_type = "e2-micro"
-
-  zone         = var.zone
-  tags = ["dev", "engineering"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        env = "dev"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}
