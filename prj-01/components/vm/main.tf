f01832cba054d95d4e833d001ce77c57b595bad4
vnnimbalkar <vaibhav.nimbalkar291@gmail.com>
2024-01-24 19:38:48 +0530

    Update main.tf

--- prj-01/components/vm/main.tf
+++ prj-01/components/vm/main.tf
@@ -2,3 +2,10 @@ provider "google" {
   project     = var.project
   region      = var.region
 }
+resource "google_storage_bucket" "auto-expire" {
+  name          = "prj-01-bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
